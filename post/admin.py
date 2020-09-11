7319a98f93543784001e58b6b2b2fdaaa468c6d0
marioalbornoz <mario.albornozc@utem.cl>
2020-09-11 14:52:19 -0300

    se agrega modelo post

---
+++ post/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import  Carrera, Alumno, Folio
+# Register your models here.
+admin.site.register(Carrera)
+admin.site.register(Alumno)
+admin.site.register(Folio)
---
+++ post/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+# from usuario.models import Carrera
+from django.contrib.auth.models import User
+import uuid
+
+
+# Create your models here.
+
+class Carrera(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    codigo = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Alumno(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    rut = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {}'.format(self.nombres, self.apellidos)
+
+class Folio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length = 1000)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    student = models.ForeignKey(Alumno, on_delete=models.CASCADE )
+
+    class Meta:
+        ordering = ['created']
+    def __str__(self):
+        return self.content
