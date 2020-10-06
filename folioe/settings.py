ff56828ba8fbf1af99c5d1cf1a4d02637b20b96b
marioalbornoz <mario.albornozc@utem.cl>
2020-10-06 13:48:14 -0300

    se aumenta tiempo de token

--- folioe/settings.py
+++ folioe/settings.py
@@ -136,7 +136,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 #APPEND_SLASH = False
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=14),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
