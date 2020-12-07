e9616411c1deaf562bd7805da4e6ce50c30c5a86
marioalbornoz <mario.albornozc@utem.cl>
2020-12-06 22:28:45 -0300

    se agrega componente editar dentro del cardperfil

---
+++ src/components/CardPerfil.jsx
@@ -0,0 +1,202 @@
+import Axios from 'axios';
+import React, { useState } from 'react'
+import { rolUsuario } from '../helpers';
+import img from '../perfil-clase.png';
+import Config from '../utils/Config';
+
+export const CardPerfil = ({user, editando, estaEditando, id}) => {
+
+    const [repeat, setRepeat] = useState(false);
+    const [datos, setDatos] = useState({
+        username:user.username,
+        first_name:"",
+        last_name:"",
+        email:user.email,
+        password1:"",
+        password2:""
+    })
+    const comparePassword = () => {
+        
+    }
+    const handleInput = (e) => {
+      setDatos({
+        ...datos,
+        [e.target.name]: e.target.value,
+      });
+      comparePassword();
+    };
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        estaEditando(!editando)
+        console.log("usuario editado");
+        const enviarDatos = async() => {
+            try {
+                const url = `${Config.listaUsuarios}${id}/`
+                const postear = await Axios.put(url, {
+                  username: datos.username.toLowerCase(),
+                  email: datos.email,
+                  password: datos.password1,
+                  first_name: datos.first_name,
+                  last_name: datos.last_name,
+                },
+                {
+                    headers: {
+                      Authorization: `JWT ${localStorage.getItem("token")}`,
+                      "Content-type": "application/json",
+                    },
+                  }
+                ); 
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        if(datos.password2.trim() === datos.password1.trim()){
+            setRepeat(false);
+            enviarDatos();
+        }else {
+          setRepeat(true);
+        }
+
+        
+    }
+    return !editando ? (
+      <div className="card shadow redondeado ml-lg-5">
+        <div className="perfil-imagen m-2">
+          <img src={img} alt="" className="img-perfil" />
+        </div>
+        <div className="col-lg-9 col-md-8 col-sm-12 pt-4 perfil-cuerpo m-3">
+          <p className="lead">
+            <span className="font-weight-bold">Usuario</span>: {user.username}
+          </p>
+          {user.first_name ? (
+            <p className="lead">
+              {" "}
+              <span className="font-weight-bold">Nombres</span>{" "}
+              {user.first_name} {user.last_name}
+            </p>
+          ) : null}
+          <p className="lead">
+            <span className="font-weight-bold">Rol de Usuario</span>:{" "}
+            {rolUsuario(user.rol)}
+          </p>
+          <p className="lead">
+            <span className="font-weight-bold">Correo</span>: {user.email}
+          </p>
+          {/* {
+              user.facultad ?<p><span className="font-weight-bold">Facultad</span>: {user.facultad}</p> : null
+            }
+            */}
+          {user.escuela ? (
+            <p className="lead text-capitalize">
+              <span className="font-weight-bold">Escuela</span>:{" "}
+              {user.escuelanombre}
+            </p>
+          ) : null}
+
+          {user.carrera ? (
+            <p className="lead">
+              <span className="font-weight-bold">Carrera</span>:{" "}
+              {user.carreranombre}
+            </p>
+          ) : null}
+        </div>
+      </div>
+    ) : (
+      <div className="card shadow redondeado ml-lg-5">
+        <div className="perfil-imagen m-2">
+          <img src={img} alt="" className="img-perfil" />
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="col-lg-9 col-md-8 col-sm-12 pt-4 perfil-cuerpo m-3"
+        >
+          <label>Usuario: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="usuario"
+            placeholder={user.username}
+            onChange={handleInput}
+            value={datos.username}
+          />
+
+          <>
+            <label>Nombres: </label>
+            <input
+              type="text"
+              name="first_name"
+              className="form-control"
+              placeholder={user.first_name}
+              onChange={handleInput}
+              value={datos.first_name}
+            />
+          </>
+          <>
+            <label>Apellidos</label>{" "}
+            <input
+              type="text"
+              name="last_name"
+              className="form-control"
+              placeholder={user.last_name}
+              onChange={handleInput}
+              value={datos.last_name}
+            />{" "}
+          </>
+          <p className="mt-3">
+            <span className="font-weight-bold ">Rol de Usuario</span>:{" "}
+            {rolUsuario(user.rol)}
+          </p>
+          <label>Correo: </label>
+          <input
+            type="text"
+            name="email"
+            placeholder={user.email}
+            className="form-control"
+            onChange={handleInput}
+            value={datos.email}
+          />
+          {/* {
+              user.facultad ?<p><span className="font-weight-bold">Facultad</span>: {user.facultad}</p> : null
+            }
+            */}
+          {user.escuela ? (
+            <p className=" text-capitalize mt-3">
+              <span className="font-weight-bold">Escuela</span>:{" "}
+              {user.escuelanombre}
+            </p>
+          ) : null}
+
+          {user.carrera ? (
+            <p className="mt-3">
+              <span className="font-weight-bold">Carrera</span>:{" "}
+              {user.carreranombre}
+            </p>
+          ) : null}
+          <label>Contrasenia:</label>
+          <input
+            className="form-control"
+            type="password"
+            name="password1"
+            onChange={handleInput}
+            value={datos.password1}
+          />
+          <label>Repita contrasenia:</label>
+          <input
+            className="form-control"
+            onChange={handleInput}
+            type="password"
+            name="password2"
+            value={datos.password2}
+          />
+          {repeat ? (
+            <div className="alert alert-danger">
+              Las claves deben ser similares
+            </div>
+          ) : null}
+         <input type="submit" value="Guardar" /> 
+
+          {/* <button onClick={estaEditando(false)}>Guardar</button> */}
+        </form>
+      </div>
+    );
+}
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,19 +1,24 @@
 import React, { useContext } from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { CardPerfil } from '../components/CardPerfil';
 import { Spinner } from '../components/Spinner';
 import { PerfilContext } from '../context/PerfilContext';
 import { UserContext } from '../context/UserContext';
-import { rolUsuario } from '../helpers';
-import img from '../perfil-clase.png';
 export const Perfil = () => {
   
   const {usuarios} = useContext(UserContext)
   // perfil entrega datos de usuario log
   const { perfil } = useContext(PerfilContext);
-  // const { username, email, rol } = perfil;
+   // estado que guarda la id desde la url para usarla en el use effect
   const [idurl, guardarId] = useState();
-    
+  //  Estado que guarda el bool si se va a editar
+  const [editando, estaEditando] = useState(false);
+  // Funcion booleana que indica si el boton editar se presiono 
+  const handleEdit = (e) => {
+    e.preventDefault();
+    estaEditando(true);
+  }
     
     useEffect(()=>{
       var URLactual = window.location.href;
@@ -31,48 +36,13 @@ export const Perfil = () => {
         <div className="row m-3 ml-5 pb-4 title">
           <h4 className="font-weight-bold mr-3">Perfil Usuario</h4>
           {
-            perfil.id === parseInt(idurl) ? <button type="submit" className="btn btn-primary size">Editar <i className="fas fa-user-edit"></i></button> : null
+            perfil.id === parseInt(idurl) ? <button onClick={(e)=>handleEdit(e)} type="submit" className="btn btn-primary size">Editar <i className="fas fa-user-edit"></i></button> : null
           }
           
         </div>
         {filtrado ? filtrado.map((user) => (
-          <div key={user.id} className="card shadow redondeado ml-lg-5">
-            <div className="perfil-imagen m-2">
-              <img src={img} alt="" className="img-perfil" />
-            </div>
-            <div className="col-lg-9 col-md-8 col-sm-12 pt-4 perfil-cuerpo m-3">
-              <p className="lead">
-                <span className="font-weight-bold">Usuario</span>:{" "}
-                {user.username}
-              </p>
-             {user.first_name ? (
-                <p className="lead">
-                  {" "}
-                  <span className="font-weight-bold">Nombres</span>{" "}
-                  {user.first_name} {user.last_name}
-                </p>
-              ) : null}
-              <p className="lead">
-                <span className="font-weight-bold">Rol de Usuario</span>:{" "}
-                {rolUsuario(user.rol)}
-              </p>
-              <p className="lead">
-                <span className="font-weight-bold">Correo</span>: {user.email}
-              </p>
-              {/* {
-              user.facultad ?<p><span className="font-weight-bold">Facultad</span>: {user.facultad}</p> : null
-            }
-            */}
-            {
-              user.escuela ?<p className="lead text-capitalize"><span className="font-weight-bold">Escuela</span>: {user.escuelanombre}</p> : null
-            }
-            
-            {
-              user.carrera ?<p className="lead"><span className="font-weight-bold">Carrera</span>: {user.carreranombre}</p> : null
-            } 
-            </div>
-            
-          </div>
+          // !editando ? <CardPerfil user={user} key={user.id} />: null
+          <CardPerfil perfil={perfil} user={user} key={user.id} editando={editando} estaEditando={estaEditando} id={idurl}/>
         )): <Spinner />}
 
       </div>
